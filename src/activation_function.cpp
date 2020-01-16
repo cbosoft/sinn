1839bff70be4ea0bc7e5373450908e09fe87f1c5
Chris <chrismjboyle@gmail.com>
2020-01-16 15:59:33 +0000

    Added activation functions

---
+++ src/activation_function.cpp
@@ -0,0 +1,8 @@
+#include "activation_function.hpp"
+
+double actfunc_identity(double x, std::vector<double> args)
+{
+  (void) args;
+
+  return x;
+}
---
+++ src/activation_function.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+typedef double (*ActivationFunction)(double, std::vector<double>);
+
+double actfunc_identity(double, std::vector<double>);
+double actfunc_heaviside(double, std::vector<double>);
+double actfunc_sigmoid(double, std::vector<double>);
+double actfunc_tanh(double, std::vector<double>);
+double actfunc_relu(double, std::vector<double>);
+double actfunc_lrelu(double, std::vector<double>);
+double actfunc_prelu(double, std::vector<double>);
+double actfunc_rrelu(double, std::vector<double>);
+double actfunc_elu(double, std::vector<double>);
+double actfunc_selu(double, std::vector<double>);
+double actfunc_srelu(double, std::vector<double>);
+double actfunc_apl(double, std::vector<double>);
+double actfunc_softplus(double, std::vector<double>);
+double actfunc_bent(double, std::vector<double>);
