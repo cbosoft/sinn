3f35db3f466bf14213cc6c86d826fa39cf2f4a52
Chris <chrismjboyle@gmail.com>
2020-01-22 13:35:39 +0000

    Prints out a bit more information: easier to see progress of training

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "neural_network.hpp"
 #include "hidden_layer.hpp"
 #include "input_layer.hpp"
@@ -24,10 +26,17 @@ int main(void)
   nn.set_input(training_inputs[0]);
   nn.print();
 
-  for (int i = 0; i < 10000 && nn.get_last_error() > 0.0; i++) {
+  for (int i = 0; i < 10000 && nn.get_last_error() > 5e-2; i++) {
     nn.train(training_inputs, training_outputs, 0.01);
+    nn.set_input(training_inputs[0]);
+    nn.print();
   }
 
-  nn.set_input(training_inputs[0]);
-  nn.print();
+  for (size_t i = 0; i < training_inputs.size(); i++) {
+    nn.set_input(training_inputs[i]);
+    nn.print();
+    std::cout << "Expected output:" << std::endl;
+    for (size_t j = 0; j < training_outputs[i].size(); j++)
+      std::cout << "  " << training_outputs[i][j] << std::endl;
+  }
 }
