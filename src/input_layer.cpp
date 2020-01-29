7bf70ba52964974e5020c5a85d70d8c3d18a7daf
Chris <chrismjboyle@gmail.com>
2020-01-29 13:10:33 +0000

    Added inputlayer construction given just a size

--- src/input_layer.cpp
+++ src/input_layer.cpp
@@ -6,12 +6,25 @@ InputLayer::InputLayer(std::vector<double> input_values)
   this->generate_neurons(input_values);
 }
 
+InputLayer::InputLayer(int n)
+{
+  this->generate_neurons(n);
+}
+
 
 void InputLayer::generate_neurons(std::vector<double> input_values)
 {
-  for (auto value : input_values)
-  {
+  for (auto value : input_values) {
     InputNeuron *n = new InputNeuron(value);
+    this->add_neuron(n);
+  }
+}
+
+
+void InputLayer::generate_neurons(int n)
+{
+  for (int i = 0; i < n; i++) {
+    InputNeuron *n = new InputNeuron(0.0);
     this->add_neuron(n);
   }
 }
--- src/input_layer.hpp
+++ src/input_layer.hpp
@@ -9,8 +9,10 @@ class InputLayer : public virtual Layer {
   public:
 
     InputLayer(std::vector<double> input_values);
+    InputLayer(int n);
     ~InputLayer() =default;
 
     void generate_neurons(std::vector<double> input_values);
+    void generate_neurons(int n);
     void set_values(std::vector<double> input_values);
 };
