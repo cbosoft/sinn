d221c30af65e0405311f462a797acc1e78ff8d3f
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-01-29 19:26:40 +0000

    troubleshooting nan errors in test2

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
 
-#include "neural_network.hpp"
-#include "hidden_layer.hpp"
-#include "input_layer.hpp"
-#include "output_layer.hpp"
+#include "sinn.hpp"
+#include "util.hpp"
 
 int main(void)
 {
   NeuralNetwork nn = NeuralNetwork();
-  nn.add_layer(new InputLayer({0.1, 0.2, 0.3}));
-  nn.add_layer(new OutputLayer(2));
+  nn.add_layer(new InputLayer(5));
+  nn.add_layer(new OutputLayer(1));
+
   std::vector<std::vector<double>> training_inputs = {
-    {1, 1, 1},
-    {2, 2, 2},
-    {3, 3, 3},
-    {4, 4, 4}
+    {1, 2, 3, 4},
+    {2, 3, 4, 5},
+    {3, 4, 5, 6},
+    {4, 5, 6, 7}
   };
+
   std::vector<std::vector<double>> training_outputs = {
-    {3, 3},
-    {6, 6},
-    {9, 9},
-    {12, 12}
+    {5},
+    {6},
+    {7},
+    {8}
   };
 
-  nn.set_input(training_inputs[0]);
-  nn.print();
+  std::cout << vec2string(nn(training_inputs[0])) << std::endl;
+  exit(0);
 
-  for (int i = 0; i < 10000 && nn.get_last_error() > 5e-2; i++) {
-    nn.train(training_inputs, training_outputs, 0.01);
-    nn.set_input(training_inputs[0]);
-    nn.print();
+  double learning_rate = 0.1;
+  double tolerance = 5e-2;
+  int epochs = 30;
+
+  for (int i = 0; i < epochs /*&& nn.get_last_error() > tolerance*/; i++) {
+    nn.train(training_inputs, training_outputs, learning_rate);
+    auto input = training_inputs[0];
+    std::vector<double> output = nn(input);
+    std::cout << i << ": " << vec2string(input) << " -> " << vec2string(output) << ", err: " << nn.get_last_error() << std::endl;
   }
 
   for (size_t i = 0; i < training_inputs.size(); i++) {
-    nn.set_input(training_inputs[i]);
-    nn.print();
-    std::cout << "Expected output:" << std::endl;
-    for (size_t j = 0; j < training_outputs[i].size(); j++)
-      std::cout << "  " << training_outputs[i][j] << std::endl;
+    auto input = training_inputs[i];
+    std::vector<double> output = nn(input);
+    std::cout << vec2string(input) << " -> " << vec2string(output) << " (" << vec2string(training_outputs[i]) << ")" << std::endl;
   }
 }
