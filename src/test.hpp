60d8bb89a91dedd30c6ca59db8ddc4924618ce49
Chris <chrismjboyle@gmail.com>
2020-01-30 09:05:52 +0000

    Added header and footer funcs to make test formatting consistent

--- src/test.hpp
+++ src/test.hpp
@@ -1,4 +1,29 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "colour.hpp"
 
-#define TEST_PASSED 0
-#define TEST_FAILED 1
+inline void test_header(std::string name)
+{
+  std::cerr << name << ":";
+}
+
+inline void pass_and_exit()
+{
+  std::cerr << " " << FG_GREEN << "PASSED" << RESET << std::endl;
+  exit(0);
+}
+
+inline void fail_and_exit()
+{
+  std::cerr << " " << BG_RED << "FAILED" << RESET << std::endl;
+  exit(1);
+}
+
+inline void test_check_and_exit(bool condition)
+{
+  if (condition)
+    pass_and_exit();
+  else
+    fail_and_exit();
+}
