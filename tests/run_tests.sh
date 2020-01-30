671d5c79e85a2acb3a991ffc509ab46fc2c7297a
Chris <chrismjboyle@gmail.com>
2020-01-30 09:38:47 +0000

    Removed print of pass/fail from test runner script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,9 +3,7 @@
 TESTS=$(ls test*)
 
 for test in $TESTS; do
-  printf "./%s" "$test"
-  ./"$test" || (printf " \u001b[41mFAILED\u001b[0m" ; exit)
-  printf " \u001b[32mPASSED\u001b[0m\n"
+  ./"$test" || exit 1
 done
 
 echo "All tests run and passed!"
