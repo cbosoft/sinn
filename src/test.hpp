9f34478857765210cd0ef6d46ee76c1d2777b011
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-01-30 21:49:34 +0000

    Added func to generate a test set of data for a given function

--- src/test.hpp
+++ src/test.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+#include <random>
+
+#include "util.hpp"
 #include "colour.hpp"
 
 inline void test_header(std::string name)
@@ -26,4 +30,17 @@ inline void test_check_and_exit(bool condition)
     pass_and_exit();
   else
     fail_and_exit();
+}
+
+typedef double (* TrainingFunction)(int );
+
+inline void get_training_set_from_function(TrainingFunction *f, int n, std::vector<double> &inputs, std::vector<double> &outputs)
+{
+  inputs.erase(inputs.begin(), inputs.end());
+  outputs.erase(outputs.begin(), outputs.end());
+  for (int i = 0; i < n; i++) {
+    double v = sinn::util::get_normal_random();
+    inputs.push_back(v);
+    outputs.push_back((*f)(v));
+  }
 }
