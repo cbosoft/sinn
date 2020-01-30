9ca0b1f7eb6419b4c4d4b3b5e204cd4d99e2b6f9
Chris <chrismjboyle@gmail.com>
2020-01-30 11:51:53 +0000

    Moved test sources to own directory

--- src/tests/linear_regression1.cpp
+++ src/tests/linear_regression1.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
-#include <sstream>
 
-#include "sinn.hpp"
-#include "test.hpp"
+#include "../sinn.hpp"
+#include "../test.hpp"
 
-int main(void)
+int main(int argc, const char **argv)
 {
-  test_header("2. next-in-series");
-
+  (void) argc;
+  test_header(argv[0]);
   sinn::NeuralNetwork nn = sinn::NeuralNetwork();
-  nn.add_layer(new sinn::InputLayer(4), 0.1);
+  nn.add_layer(new sinn::InputLayer(1), 0.1);
   nn.add_layer(new sinn::OutputLayer(1), 0.1);
 
   std::vector<std::vector<double>> training_inputs = {
-    {1, 2, 3, 4},
-    {2, 3, 4, 5},
-    {3, 4, 5, 6},
-    {4, 5, 6, 7}
+    {1},
+    {2},
+    {3},
+    {4}
   };
 
   std::vector<std::vector<double>> training_outputs = {
+    {3},
+    {6},
+    {9},
+    {12}
+  };
+
+  std::vector<std::vector<double>> checking_inputs = {
     {5},
     {6},
     {7},
     {8}
   };
 
-  std::vector<std::vector<double>> checking_inputs = {
-    { 5,  6,  7,  8},
-    { 6,  7,  8,  9},
-    { 7,  8,  9, 10},
-    { 8,  9, 10, 11}
-  };
-
   std::vector<std::vector<double>> checking_outputs = {
-    {9},
-    {10},
-    {11},
-    {12}
+    {15},
+    {18},
+    {21},
+    {24}
   };
 
-
-
-  double learning_rate = 0.1;
+  double learning_rate = 10;
   double tolerance = 0.5;
-  int epochs = 10000;
+  int epochs = 30;
 
   for (int i = 0; i < epochs && nn.get_last_error() > tolerance; i++) {
     nn.train(training_inputs, training_outputs, learning_rate);
@@ -55,16 +52,19 @@ int main(void)
 
   // for (size_t i = 0; i < training_inputs.size(); i++) {
   //   auto input = training_inputs[i];
+  //   auto check = training_outputs[i];
   //   std::vector<double> output = nn(input);
-  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(training_outputs[i]) << ")" << std::endl;
+  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(check) << ")" << std::endl;
   // }
   // for (size_t i = 0; i < checking_inputs.size(); i++) {
   //   auto input = checking_inputs[i];
+  //   auto check = checking_outputs[i];
   //   std::vector<double> output = nn(input);
-  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(checking_outputs[i]) << ")" << std::endl;
+  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(check) << ")" << std::endl;
   // }
   // std::cout << nn.get_error(checking_inputs, checking_outputs) << std::endl;
   // std::cout << nn.get_last_error() << std::endl;
 
   test_check_and_exit(nn.get_error(checking_inputs, checking_outputs) < tolerance);
 }
+
