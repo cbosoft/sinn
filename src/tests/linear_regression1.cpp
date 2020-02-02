b081bce76c3a58543326d72edd2ea6d0177c3071
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-02 17:40:46 +0000

    Updated tests for new add layer signature

--- src/tests/linear_regression1.cpp
+++ src/tests/linear_regression1.cpp
@@ -8,8 +8,8 @@ int main(int argc, const char **argv)
   (void) argc;
   test_header(argv[0]);
   sinn::NeuralNetwork nn = sinn::NeuralNetwork();
-  nn.add_layer(new sinn::InputLayer(1), 0.1);
-  nn.add_layer(new sinn::OutputLayer(1), 0.1);
+  nn.add_layer_normal(new sinn::InputLayer(1), 1.0, 0.5);
+  nn.add_layer_normal(new sinn::OutputLayer(1), 1.0, 0.5);
 
   std::vector<std::vector<double>> training_inputs = {
     {1},
@@ -41,13 +41,13 @@ int main(int argc, const char **argv)
 
   double learning_rate = 10;
   double tolerance = 0.5;
-  int epochs = 30;
+  int epochs = 10000;
 
-  for (int i = 0; i < epochs && nn.get_last_error() > tolerance; i++) {
+  for (int i = 0; i < epochs && nn.get_last_error() > tolerance*0.1; i++) {
     nn.train(training_inputs, training_outputs, learning_rate);
-    //auto input = training_inputs[0];
-    //std::vector<double> output = nn(input);
-    //std::cout << i << ": " << vec2string(input) << " -> " << vec2string(output) << ", err: " << nn.get_last_error() << std::endl;
+    // auto input = training_inputs[0];
+    // std::vector<double> output = nn(input);
+    // std::cout << i << ": " << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << ", err: " << nn.get_last_error() << std::endl;
   }
 
   // for (size_t i = 0; i < training_inputs.size(); i++) {
--- src/tests/next_in_series1.cpp
+++ src/tests/next_in_series1.cpp
@@ -10,8 +10,8 @@ int main(int argc, const char **argv)
   test_header(argv[0]);
 
   sinn::NeuralNetwork nn = sinn::NeuralNetwork();
-  nn.add_layer(new sinn::InputLayer(4), 0.1);
-  nn.add_layer(new sinn::OutputLayer(1), 0.1);
+  nn.add_layer_weighted(new sinn::InputLayer(4), 0.1);
+  nn.add_layer_weighted(new sinn::OutputLayer(1), 0.1);
 
   std::vector<std::vector<double>> training_inputs = {
     {1, 2, 3, 4},
@@ -45,27 +45,27 @@ int main(int argc, const char **argv)
 
   double learning_rate = 0.1;
   double tolerance = 0.5;
-  int epochs = 10000;
+  //int epochs = 10000;
 
-  for (int i = 0; i < epochs && nn.get_last_error() > tolerance; i++) {
+  for (int i = 0; /*i < epochs && */nn.get_last_error() > tolerance*0.1; i++) {
     nn.train(training_inputs, training_outputs, learning_rate);
-    //auto input = training_inputs[0];
-    //std::vector<double> output = nn(input);
-    //std::cout << i << ": " << vec2string(input) << " -> " << vec2string(output) << ", err: " << nn.get_last_error() << std::endl;
+    auto input = training_inputs[0];
+    std::vector<double> output = nn(input);
+    std::cout << i << ": " << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << ", err: " << nn.get_last_error() << std::endl;
   }
 
-  // for (size_t i = 0; i < training_inputs.size(); i++) {
-  //   auto input = training_inputs[i];
-  //   std::vector<double> output = nn(input);
-  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(training_outputs[i]) << ")" << std::endl;
-  // }
-  // for (size_t i = 0; i < checking_inputs.size(); i++) {
-  //   auto input = checking_inputs[i];
-  //   std::vector<double> output = nn(input);
-  //   std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(checking_outputs[i]) << ")" << std::endl;
-  // }
-  // std::cout << nn.get_error(checking_inputs, checking_outputs) << std::endl;
-  // std::cout << nn.get_last_error() << std::endl;
+  for (size_t i = 0; i < training_inputs.size(); i++) {
+    auto input = training_inputs[i];
+    std::vector<double> output = nn(input);
+    std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(training_outputs[i]) << ")" << std::endl;
+  }
+  for (size_t i = 0; i < checking_inputs.size(); i++) {
+    auto input = checking_inputs[i];
+    std::vector<double> output = nn(input);
+    std::cout << sinn::util::vec2string(input) << " -> " << sinn::util::vec2string(output) << " (" << sinn::util::vec2string(checking_outputs[i]) << ")" << std::endl;
+  }
+  std::cout << nn.get_error(checking_inputs, checking_outputs) << std::endl;
+  std::cout << nn.get_last_error() << std::endl;
 
   test_check_and_exit(nn.get_error(checking_inputs, checking_outputs) < tolerance);
 }
